36838ee190af2264dd6418f71baa0951be1426bc
Samuel Souza <saamuelfelipe@gmail.com>
2024-06-09 16:27:12 -0300

    Inseridos os primeiros cenários das funcionalidades Pesquisar e listar filmes,

---
+++ testesApi/cypress/e2e/filme/listaDeFilme.cy.js
@@ -0,0 +1,7 @@
+/// <reference types= "cypress"/>
+
+import { faker } from "@faker-js/faker";
+
+describe("Listar filme", () => {
+  it("Deve ser possível pesquisar um filme pelo título completo", () => {});
+});
---
+++ testesApi/cypress/e2e/filme/pesquisaFilme.cy.js
@@ -0,0 +1,24 @@
+/// <reference types= "cypress"/>
+
+import { faker } from "@faker-js/faker";
+
+describe("Pesquisar Filme", () => {
+  it("Deve ser possível pesquisar um filme pelo título completo", () => {});
+
+it('Deve ser possível pesquisar um filme com título parcial', () => {
+    
+});
+
+it('Deve ser possível pesquisar um filme com erro de digitação', () => {
+    
+});
+
+it('', () => {
+    
+});
+
+it('', () => {
+    
+});
+
+});
