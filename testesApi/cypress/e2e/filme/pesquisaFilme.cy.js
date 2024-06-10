400f6d65570b6eb05a054873e6dee9c05e4fcf2f
Samuel Souza <saamuelfelipe@gmail.com>
2024-06-09 22:40:14 -0300

    criação dos primeiros cenários de teste.

--- testesApi/cypress/e2e/filme/pesquisaFilme.cy.js
+++ testesApi/cypress/e2e/filme/pesquisaFilme.cy.js
@@ -3,20 +3,47 @@
 import { faker } from "@faker-js/faker";
 
 describe("Pesquisar Filme", () => {
-  // autenticar como adm
-  // criar um objeto filme (completo)
-  // criar o filme
-  // buscar o filme pelo Id e guardar em uma variável
+  let usuarioAdmin;
+  let filme;
+
+  before(() => {
+    cy.criarUsuarioAdmin().then((response) => {
+      usuarioAdmin = { ...response };
+      const anoAtual = new Date().getFullYear();
+      const filmeObj = {
+        title: faker.lorem.words(10),
+        genre: faker.lorem.words(3),
+        description: faker.lorem.words(20),
+        durationInMinutes: Math.floor(Math.random() * (140 - 100 + 1)) + 100,
+        releaseYear: Math.floor(Math.random() * (anoAtual - 1895 + 1)) + 1895,
+      };
+      cy.cadastrarFilmeComBody(response.token, filmeObj).then((response) => {
+        filme = { ...response };
+      });
+    });
+  });
 
   it("Deve ser possível pesquisar um filme pelo título completo", () => {
-    cy.buscarFilme()
+    cy.buscarFilme(filme.title).then((response) => {
+      expect(response).to.equal(filme.id);
+    });
   });
 
   it("Deve ser possível pesquisar um filme com título parcial", () => {
-    cy.buscarFilme()
+    const tituloSplit = filme.title.split(" ");
+    const tituloParcial = `${tituloSplit[0]} ${tituloSplit[1]} ${tituloSplit[2]}`;
+    cy.buscarFilme(tituloParcial).then((response) => {
+      expect(response).to.equal(filme.id);
+    });
   });
 
-  it("Deve ser possível pesquisar um filme com erro de digitação", () => {});
+  // it("Deve ser possível pesquisar um filme com erro de digitação", () => {
+  //   const tituloSplit = filme.title.split(" ");
+  //   const tituloParcial = `${tituloSplit[0]} ${tituloSplit[1]} ${tituloSplit[2]}S`;
+  //   cy.buscarFilme(tituloParcial).then((response) => {
+  //     expect(response).to.equal(filme.id);
+  //   });
+  // });
 
   it("Deve ser possível pesquisar um filme com letras maiúsculas", () => {});
 
@@ -45,5 +72,4 @@ describe("Pesquisar Filme", () => {
   it("Deve ser possível um usuário crítico  realizar uma pesquisa no catálogo de filmes", () => {});
 
   it("Deve ser possível um usuário administrador realizar uma pesquisa no catálogo de filmes", () => {});
-
 });
