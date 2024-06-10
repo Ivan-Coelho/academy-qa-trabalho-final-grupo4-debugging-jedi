60bab67f4ec549cc987e9552e710d6097d936db2
Samuel Souza <saamuelfelipe@gmail.com>
2024-06-10 11:33:30 -0300

    Adiciona a estrutura do cenário pesquisa pelo filme completo.

---
+++ testesFrontend/cypress/support/steps/filme/pesquisaFilme.step.js
@@ -0,0 +1,22 @@
+import { Given, When, Then, Before, After} from '@badeball/cypress-cucumber-preprocessor';
+import FilmesPage from '../pages/filmes.page';
+import RegistroPage from '../pages/registro.page';
+import { faker } from '@faker-js/faker';
+
+
+Given('que acessei o site', function () {
+    cy.visit('')
+});
+
+Given ('que o usuário inseriu o título completo do filme na caixa de pesquisa', function () {
+    
+})
+
+When('acionar o no botão de buscar', function () {
+    
+})
+
+Then('o sistema deve retornar o filme correspondente ao título completo', function () {
+    
+})
+
