6bbfb834fed95b24b72ba3d2fa8f655f9c002e7e
Samuel Souza <saamuelfelipe@gmail.com>
2024-06-10 12:32:53 -0300

    Adiciona o cenário de listagem de filme por usuario não logado.

--- testesFrontend/cypress/support/steps/filme/listaDeFilme.step.js
+++ testesFrontend/cypress/support/steps/filme/listaDeFilme.step.js
@@ -3,4 +3,16 @@ import { Given, When, Then, Before, After} from '@badeball/cypress-cucumber-prep
 //Importar Page Objects para desenvolver os testes
 import { faker } from '@faker-js/faker';
 
+Given('que acessei o site Raro Filmes', function () {
+});
 
+Given('que um usuário não logado acessou a funcionalidade de listagem de filmes', function () {
+});
+
+When('ele visualizar a lista de filmes', function () {
+    
+})
+
+Then('o sistema deve exibir as informações sumarizadas de todos os filmes cadastrados', function () {
+    
+})
