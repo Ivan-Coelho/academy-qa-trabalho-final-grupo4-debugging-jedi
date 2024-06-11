9c943f82f7a56696870ed636a006d34d430be2e5
Samuel Souza <saamuelfelipe@gmail.com>
2024-06-11 08:42:46 -0300

    Adiciona estrutura inicial step atualizar filmes.

---
+++ testesFrontend/cypress/support/steps/filme/atualizacaoFilme.step.js
@@ -0,0 +1,8 @@
+import { Given, When, Then, Before, After} from '@badeball/cypress-cucumber-preprocessor';
+
+//Importar Page Objects para desenvolver os testes.
+import { faker } from '@faker-js/faker';
+
+
+Given('que acessei o site Raro Filmes', function () {
+});
