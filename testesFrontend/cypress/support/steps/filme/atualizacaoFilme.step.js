b62dd3f0b807be9dd7879a31341e31a874b4876a
Samuel Souza <saamuelfelipe@gmail.com>
2024-06-11 08:44:36 -0300

    Adiciona um cenário modelo cucumber preprocessor.

--- testesFrontend/cypress/support/steps/filme/atualizacaoFilme.step.js
+++ testesFrontend/cypress/support/steps/filme/atualizacaoFilme.step.js
@@ -6,3 +6,19 @@ import { faker } from '@faker-js/faker';
 
 Given('que acessei o site Raro Filmes', function () {
 });
+
+Given ('que um usuário administrador autenticado acessou o Backend do sistema', function () {
+    
+})
+
+When('selecionar a opção de atualizar um filme', function () {
+    
+})
+
+When('fornecer todas as informações necessárias', function () {
+    
+})
+
+Then('o sistema deverá permitir que o administrador atualize as informações do filme', function () {
+    
+})
