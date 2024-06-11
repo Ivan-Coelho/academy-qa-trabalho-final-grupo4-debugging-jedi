0da9b3034f4cc7c5d06612959c61c0ac9a9f611d
Samuel Souza <saamuelfelipe@gmail.com>
2024-06-11 09:17:02 -0300

    Adiciona cenario

--- testesFrontend/cypress/support/steps/filme/pesquisaFilme.step.js
+++ testesFrontend/cypress/support/steps/filme/pesquisaFilme.step.js
@@ -3,3 +3,18 @@ import { Given, When, Then, Before, After} from '@badeball/cypress-cucumber-prep
 //Importar Page Objects para desenvolver os testes
 import { faker } from '@faker-js/faker';
 
+Given('que acessei o site Raro Filmes', function () {
+});
+
+Given ('que o usuário inseriu o título completo do filme na caixa de pesquisa', function () {
+    
+})
+
+When('acionar o recurso de buscar', function () {
+    
+})
+
+Then('o sistema deve retornar o filme correspondente ao título completo', function () {
+    
+})
+
