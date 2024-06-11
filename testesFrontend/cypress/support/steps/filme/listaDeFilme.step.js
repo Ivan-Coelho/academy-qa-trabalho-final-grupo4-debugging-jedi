e9f31d00f59443715b649c9760be9cadbfb9b42e
Samuel Souza <saamuelfelipe@gmail.com>
2024-06-11 16:17:48 -0300

    Adiciona código ao cenário de teste usuário não logado.

--- testesFrontend/cypress/support/steps/filme/listaDeFilme.step.js
+++ testesFrontend/cypress/support/steps/filme/listaDeFilme.step.js
@@ -3,76 +3,91 @@ import { Given, When, Then, Before, After} from '@badeball/cypress-cucumber-prep
 //Importar aqui os Page Objects para desenvolver os testes
 import { faker } from '@faker-js/faker';
 
-beforeEach(() => {
+// beforeEach(() => {
+//     cy.visit('')
+// });
+
+Given('que um usuário não logado acessou a página inicial', function () {
     cy.visit('')
-});
+    });
 
-Given('que um usuário não logado acessou a funcionalidade de listagem de filmes', function () {
+When('ele requisitar a opção filmes', function () {
     cy.get('.navbar-content > :nth-child(3)').click()
-});
-
-When('ele visualizar a lista de filmes', function () {
-    cy.get('.featured-movies > .section-header').contains('Filmes em destaque').should('be.visible')    
 })
 
-Then('o sistema deve exibir as informações sumarizadas de todos os filmes cadastrados', function () {
+Then('deve ser possivél visualizar as informações sumarizadas de filmes', function () {
+    cy.get('.featured-movies > .section-header').contains('Filmes em destaque').should('be.visible')
+})
+
+// Given('que um usuário não logado acessou a funcionalidade de listagem de filmes', function () {
+//     cy.get('.navbar-content > :nth-child(3)').click()
+// });
+
+// When('ele visualizar a lista de filmes', function () {
+//     cy.get('.featured-movies > .section-header').contains('Filmes em destaque').should('be.visible')    
+// })
+
+// Then('o sistema deve exibir as informações sumarizadas de todos os filmes cadastrados', function () {
+//     cy.get('.featured-movies > .carousel-container > .carousel-data > [href="/movies/37"] > .movie-card-footer').to.have.property("description");
+// })
+
+// Given ('que um usuário comum acessou a funcionalidade de listagem de filmes', function () {
     
-})
+// })
 
-Given ('que um usuário comum acessou a funcionalidade de listagem de filmes', function () {
+
+// Given ('que um usuário crítico acessou a funcionalidade de listagem de filmes', function () {
     
-})
+// })
 
 
-Given ('que um usuário crítico acessou a funcionalidade de listagem de filmes', function () {
+// Given ('que um usuário administrador acessou a funcionalidade de listagem de filmes', function () {
     
-})
+// })
 
 
-Given ('que um usuário administrador acessou a funcionalidade de listagem de filmes', function () {
+// Given ('que um usuário acessou a página inicial', function () {
     
-})
+// })
 
-
-Given ('que um usuário acessou a página inicial', function () {
+// When('ele selecionar a opção filmes', function () {
     
-})
+// })
 
-
-Then('o card de um filme deverá conter o título do mesmo', function () {
+// Then('o card de um filme deverá conter o título do mesmo', function () {
     
-})
+// })
 
 
-Then('o card de um filme deverá conter a descrição correspondente ao mesmo', function () {
+// Then('o card de um filme deverá conter a descrição correspondente ao mesmo', function () {
     
-})
+// })
 
 
 
-Then('o card de um filme deverá conter a sua nota geral', function () {
+// Then('o card de um filme deverá conter a sua nota geral', function () {
     
-})
+// })
 
 
 
-Then('os filmes cadastrados deverão ser listados conforme sua ordem de cadastro', function () {
+// Then('os filmes cadastrados deverão ser listados conforme sua ordem de cadastro', function () {
     
-})
+// })
 
 
-Then('deverá haver uma opção de visualizar filmes mais bem avaliados', function () {
+// Then('deverá haver uma opção de visualizar filmes mais bem avaliados', function () {
     
-})
+// })
 
-Given ('que um usuário acessou a lista de filmes', function () {
+// Given ('que um usuário acessou a lista de filmes', function () {
     
-})
+// })
 
-When('houver mais de 5 filmes cadastrados', function () {
+// When('houver mais de 5 filmes cadastrados', function () {
     
-})
+// })
 
-Then('deverá existir a opção de paginação para explorar os filmes da lista', function () {
+// Then('deverá existir a opção de paginação para explorar os filmes da lista', function () {
     
-})
+// })
