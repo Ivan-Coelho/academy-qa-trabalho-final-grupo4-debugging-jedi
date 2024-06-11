63328a9d22aa24b80baaad2c00a01985c4caf014
Samuel Souza <saamuelfelipe@gmail.com>
2024-06-11 08:22:44 -0300

    Adiciona before com criação de usuario admin

--- testesApi/cypress/e2e/filme/pesquisaFilme.cy.js
+++ testesApi/cypress/e2e/filme/pesquisaFilme.cy.js
@@ -4,4 +4,22 @@ import { faker } from "@faker-js/faker";
 
 describe("Pesquisar Filme", () => {
   let usuarioAdmin;
-  let filme; })
+  let filme;
+
+  before(() => {
+    cy.criarUsuarioAdmin().then((response) => {
+      usuarioAdmin = { ...response };
+      const anoAtual = new Date().getFullYear();
+      const filmeObj = {
+        title: faker.lorem.words(10),
+        genre: faker.lorem.words(3),
+        description: faker.lorem.words(20),
+        durationInMinutes: Math.floor(Math.random() * (140 - 100 + 1)) + 100,
+        releaseYear: Math.floor(Math.random() * (anoAtual - 1895 + 1)) + 1895,
+      };
+      cy.cadastrarFilmeComBody(response.token, filmeObj).then((response) => {
+        filme = { ...response };
+      });
+    });
+  });
+})
