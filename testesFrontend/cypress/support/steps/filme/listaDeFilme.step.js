664df81a02ebccd6d1f63cab60bb5cc19dae3623
Samuel Souza <saamuelfelipe@gmail.com>
2024-06-11 15:26:00 -0300

    Refatorando código eliminando linhas com frases duplicadas.

--- testesFrontend/cypress/support/steps/filme/listaDeFilme.step.js
+++ testesFrontend/cypress/support/steps/filme/listaDeFilme.step.js
@@ -3,14 +3,16 @@ import { Given, When, Then, Before, After} from '@badeball/cypress-cucumber-prep
 //Importar aqui os Page Objects para desenvolver os testes
 import { faker } from '@faker-js/faker';
 
-Given('que acessei o site Raro Filmes', function () {
+beforeEach(() => {
+    cy.visit('')
 });
 
 Given('que um usuário não logado acessou a funcionalidade de listagem de filmes', function () {
+    cy.get('.navbar-content > :nth-child(3)').click()
 });
 
 When('ele visualizar a lista de filmes', function () {
-    
+    cy.get('.featured-movies > .section-header').contains('Filmes em destaque').should('be.visible')    
 })
 
 Then('o sistema deve exibir as informações sumarizadas de todos os filmes cadastrados', function () {
@@ -21,93 +23,43 @@ Given ('que um usuário comum acessou a funcionalidade de listagem de filmes', f
     
 })
 
-When('ele visualizar a lista de filmes', function () {
-    
-})
-
-Then('o sistema deve exibir as informações sumarizadas de todos os filmes cadastrados', function () {
-    
-})
 
 Given ('que um usuário crítico acessou a funcionalidade de listagem de filmes', function () {
     
 })
 
-When('ele visualizar a lista de filmes', function () {
-    
-})
-
-Then('o sistema deve exibir as informações sumarizadas de todos os filmes cadastrados', function () {
-    
-})
 
 Given ('que um usuário administrador acessou a funcionalidade de listagem de filmes', function () {
     
 })
 
-When('ele visualizar a lista de filmes', function () {
-    
-})
-
-Then('o sistema deve exibir as informações sumarizadas de todos os filmes cadastrados', function () {
-    
-})
 
 Given ('que um usuário acessou a página inicial', function () {
     
 })
 
-When('ele selecionar a opção filmes', function () {
-    
-})
 
 Then('o card de um filme deverá conter o título do mesmo', function () {
     
 })
 
-Given ('que um usuário acessou a página inicial', function () {
-    
-})
-
-When('ele selecionar a opção filmes', function () {
-    
-})
 
 Then('o card de um filme deverá conter a descrição correspondente ao mesmo', function () {
     
 })
 
-Given ('que um usuário acessou a página inicial', function () {
-    
-})
 
-When('ele selecionar a opção filmes', function () {
-    
-})
 
 Then('o card de um filme deverá conter a sua nota geral', function () {
     
 })
 
-Given ('que um usuário acessou a página inicial', function () {
-    
-})
 
-When('ele selecionar a opção filmes', function () {
-    
-})
 
 Then('os filmes cadastrados deverão ser listados conforme sua ordem de cadastro', function () {
     
 })
 
-Given ('que um usuário acessou a página inicial', function () {
-    
-})
-
-When('ele selecionar a opção filmes', function () {
-    
-})
 
 Then('deverá haver uma opção de visualizar filmes mais bem avaliados', function () {
     
