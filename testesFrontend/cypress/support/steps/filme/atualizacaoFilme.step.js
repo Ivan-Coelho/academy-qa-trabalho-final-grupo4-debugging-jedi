1272ed854cf46ae3ddbd6f1930263e89377a3421
Samuel Souza <saamuelfelipe@gmail.com>
2024-06-11 08:52:54 -0300

    adiciona cenario

--- testesFrontend/cypress/support/steps/filme/atualizacaoFilme.step.js
+++ testesFrontend/cypress/support/steps/filme/atualizacaoFilme.step.js
@@ -281,3 +281,14 @@ Then('o sistema deve informar que o filme foi atualizado com sucesso.', function
     
 })
 
+Given ('que um usuário administrador autenticado acessou o Backend do sistema', function () {
+    
+})
+
+When('atualizar um filme com menos de 1 minuto de duração', function () {
+    
+})
+
+Then('o sistema deve informar que o filme foi atualizado com sucesso.', function () {
+    
+})
