341cf80d163ef6d2512313d5cadf54f88497825b
Samuel Souza <saamuelfelipe@gmail.com>
2024-06-11 08:55:01 -0300

    adiciona cenario

--- testesFrontend/cypress/support/steps/filme/listaDeFilme.step.js
+++ testesFrontend/cypress/support/steps/filme/listaDeFilme.step.js
@@ -16,3 +16,15 @@ When('ele visualizar a lista de filmes', function () {
 Then('o sistema deve exibir as informações sumarizadas de todos os filmes cadastrados', function () {
     
 })
+
+Given ('que um usuário comum acessou a funcionalidade de listagem de filmes', function () {
+    
+})
+
+When('ele visualizar a lista de filmes', function () {
+    
+})
+
+Then('o sistema deve exibir as informações sumarizadas de todos os filmes cadastrados', function () {
+    
+})
