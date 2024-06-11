03be14db47ea81088192e60ab69b508c79d77229
Samuel Souza <saamuelfelipe@gmail.com>
2024-06-11 08:18:03 -0300

    Adiciona cenário de testes na funcionalidade lista de filmes.

---
+++ testesApi/cypress/e2e/filme/listaDeFilme.cy.js
@@ -0,0 +1,26 @@
+<reference types= "cypress"/>
+
+import { faker } from "@faker-js/faker";
+
+describe("Listar filme", () => {
+  it("Deve ser possível um usuário não logado consultar a lista de filmes", () => {});
+
+  it("Deve ser possível um usuário comum consultar a lista de filmes", () => {});
+
+  it("Deve ser possível um usuário crítico consultar a lista de filmes", () => {});
+
+  it("Deve ser possível um usuário administrador consultar a lista de filmes", () => {});
+  
+  it("Deve ser possível visualizar o título de um filme ao listar filmes", () => {});
+  
+  it("Deve ser possível visualizar a descrição de um filme listado", () => {});
+  
+  it("Deve ser possível visualizar a nota de um filme listado", () => {});
+  
+  it("Deve ser possível vizualizar filmes listados por ordem de cadastro", () => {});
+  
+  it("Deve ser possível vizualizar filmes listados por nota (mais avaliados para menos avaliados)", () => {});
+  
+  it("Deve ser possível realizar a paginação para explorar os filmes cadastrados", () => {});
+  
+});
