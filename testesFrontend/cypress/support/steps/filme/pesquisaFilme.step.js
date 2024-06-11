7528c1f2a6b519c97ddb6d5ecf9de8ff0318abd4
Samuel Souza <saamuelfelipe@gmail.com>
2024-06-11 09:16:19 -0300

    insere configurações iniciais em steps pesquisa de filmes.

---
+++ testesFrontend/cypress/support/steps/filme/pesquisaFilme.step.js
@@ -0,0 +1,5 @@
+import { Given, When, Then, Before, After} from '@badeball/cypress-cucumber-preprocessor';
+
+//Importar Page Objects para desenvolver os testes
+import { faker } from '@faker-js/faker';
+
