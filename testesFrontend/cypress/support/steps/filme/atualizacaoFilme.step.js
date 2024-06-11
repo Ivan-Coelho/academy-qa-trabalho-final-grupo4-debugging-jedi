1e958c5b62bc21796898d7cfef2a776cfe700a2a
Samuel Souza <saamuelfelipe@gmail.com>
2024-06-11 08:45:58 -0300

    Adiciona cenario

--- testesFrontend/cypress/support/steps/filme/atualizacaoFilme.step.js
+++ testesFrontend/cypress/support/steps/filme/atualizacaoFilme.step.js
@@ -51,6 +51,16 @@ Then('o sistema deve impedir o acesso e exibir uma mensagem de erro.', function 
     
 })
 
+Given ('que um usuário comum tenta atualizar um filme', function () {
+    
+})
 
+When('acessar a funcionalidade de atualização de filme', function () {
+    
+})
+
+Then('o sistema deve impedir o acesso e exibir uma mensagem de erro.', function () {
+    
+})
 
 
