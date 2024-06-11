be162b3ae38a66146e1badd8d4f1ace92392b9d1
Samuel Souza <saamuelfelipe@gmail.com>
2024-06-11 08:54:46 -0300

    adiciona cenario

--- testesFrontend/cypress/support/steps/filme/listaDeFilme.step.js
+++ testesFrontend/cypress/support/steps/filme/listaDeFilme.step.js
@@ -5,3 +5,14 @@ import { faker } from '@faker-js/faker';
 
 Given('que acessei o site Raro Filmes', function () {
 });
+
+Given('que um usuário não logado acessou a funcionalidade de listagem de filmes', function () {
+});
+
+When('ele visualizar a lista de filmes', function () {
+    
+})
+
+Then('o sistema deve exibir as informações sumarizadas de todos os filmes cadastrados', function () {
+    
+})
