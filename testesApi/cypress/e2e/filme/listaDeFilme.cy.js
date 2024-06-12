5f7d26a92dacc021884c8f878ec08483d54a0684
Samuel Souza <saamuelfelipe@gmail.com>
2024-06-12 17:29:27 -0300

    Insere cenário de testes am funcionalidade lista de API.

--- testesApi/cypress/e2e/filme/listaDeFilme.cy.js
+++ testesApi/cypress/e2e/filme/listaDeFilme.cy.js
@@ -1,24 +1,98 @@
 import { faker } from "@faker-js/faker";
 
 describe("Listar filme", () => {
-  it("Deve ser possível um usuário não logado consultar a lista de filmes", () => {});
+  let usuarioAdmin;
+  let usuarioComum;
+  let usuarioCritico;
+  let filme;
 
-  it("Deve ser possível um usuário comum consultar a lista de filmes", () => {});
+  before(() => {
+    cy.criarUsuarioAdmin().then((response) => {
+      usuarioAdmin = { ...response };
+      const anoAtual = new Date().getFullYear();
+      const filmeObj = {
+        title: faker.lorem.words(10),
+        genre: faker.lorem.words(3),
+        description: faker.lorem.words(20),
+        durationInMinutes: Math.floor(Math.random() * (140 - 100 + 1)) + 100,
+        releaseYear: Math.floor(Math.random() * (anoAtual - 1895 + 1)) + 1895,
+      };
+      cy.cadastrarFilmeComBody(response.token, filmeObj).then((response) => {
+        filme = { ...response };
+      });
+    });
 
-  it("Deve ser possível um usuário crítico consultar a lista de filmes", () => {});
+    cy.criarUsuario().then((response) => {
+      usuarioComum = { ...response };
+    });
 
-  it("Deve ser possível um usuário administrador consultar a lista de filmes", () => {});
-  
-  it("Deve ser possível visualizar o título de um filme ao listar filmes", () => {});
-  
-  it("Deve ser possível visualizar a descrição de um filme listado", () => {});
-  
-  it("Deve ser possível visualizar a nota de um filme listado", () => {});
-  
-  it("Deve ser possível vizualizar filmes listados por ordem de cadastro", () => {});
-  
-  it("Deve ser possível vizualizar filmes listados por nota (mais avaliados para menos avaliados)", () => {});
-  
-  it("Deve ser possível realizar a paginação para explorar os filmes cadastrados", () => {});
-  
+    cy.criarUsuarioCritico().then((response) => {
+      usuarioCritico = { ...response };
+    });
+  });
+
+  it("Deve ser possível um usuário não logado consultar a lista de filmes", () => {
+    let filmeNomeIncorreto = `${filme.title} nome errado`;
+    cy.buscarListaFilme().then((response) => {
+      expect(response.status).to.equal(200);
+      expect(response.body).to.be.an("array");
+    });
+  });
+
+  it("Deve ser possível um usuário comum consultar a lista de filmes", () => {
+    cy.criarUsuario().then((response) => {
+      cy.buscarListaFilme(response.token).then((response) => {
+        expect(response.status).to.equal(200);
+        expect(response.body).to.be.an("array");
+      });
+    });
+  });
+
+  it("Deve ser possível um usuário crítico consultar a lista de filmes", () => {
+    cy.criarUsuarioCritico().then((response) => {
+      cy.buscarListaFilme(response.token).then((response) => {
+        expect(response.status).to.equal(200);
+        expect(response.body).to.be.an("array");
+      });
+    });
+  });
+
+  it("Deve ser possível um usuário administrador consultar a lista de filmes", () => {
+    cy.buscarListaFilme(usuarioAdmin.token).then((response) => {
+      expect(response.status).to.equal(200);
+      expect(response.body).to.be.an("array");
+    });
+  });
+
+  it("Deve ser possível visualizar o título de um filme ao listar filmes", () => {
+    cy.buscarListaFilme(usuarioAdmin.token).then((response) => {
+      expect(response.status).to.equal(200);
+      expect(response.body).to.be.an("array");
+      expect(response.body[0].title).to.be.an("string");
+    });
+  });
+
+  it("Deve ser possível visualizar a descrição de um filme listado", () => {
+    1;
+    cy.buscarListaFilme(usuarioAdmin.token).then((response) => {
+      expect(response.status).to.equal(200);
+      expect(response.body).to.be.an("array");
+      expect(response.body[0].description).to.be.an("string");
+    });
+  });
+
+  it("Deve ser possível visualizar a nota de um filme listado", () => {
+    cy.buscarListaFilme(usuarioAdmin.token).then((response) => {
+      expect(response.status).to.equal(200);
+      expect(response.body).to.be.an("array");
+      expect(response.body[0].totalRating).to.be.an("number");
+    });
+  });
+
+  it("Deve ser possível vizualizar filmes listados por nota (mais avaliados para menos avaliados)", () => {
+    cy.buscarListaFilme(usuarioAdmin.token, true).then((response) => {
+      expect(response.status).to.equal(200);
+      expect(response.body).to.be.an("array");
+    });
+  });
 });
