a82fa272381ff9661349b1c61c6e0c1af3413eeb
Samuel Souza <saamuelfelipe@gmail.com>
2024-06-12 09:23:14 -0300

    Adiciona cenário de teste de um usuário critico pesquisando nocatalogo de filmes.

--- testesApi/cypress/e2e/filme/pesquisaFilme.cy.js
+++ testesApi/cypress/e2e/filme/pesquisaFilme.cy.js
@@ -119,6 +119,16 @@ describe("Pesquisar Filme", () => {
   });
 
   
+  it("Deve ser possível um usuário crítico  realizar uma pesquisa no catálogo de filmes", () => {
+    cy.criarUsuarioCritico().then((response) => {
+      usuarioCritico = { ...response };
+      cy.buscarFilmeResponseCompleto(filme.title, response.token).then(
+        (response) => {
+          expect(response.body[0].id).to.equal(filme.id);
+        }
+      );
+    });
+  });
 
   it("Deve ser possível um usuário crítico  realizar uma pesquisa no catálogo de filmes", () => {});
 
