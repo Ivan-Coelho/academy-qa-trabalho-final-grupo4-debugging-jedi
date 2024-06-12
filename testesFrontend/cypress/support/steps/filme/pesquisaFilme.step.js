4b47187859282a758fbed5faf1798c0ac6d36fb5
Samuel Souza <saamuelfelipe@gmail.com>
2024-06-12 17:18:21 -0300

    Ajustando configurações iniciais cenario de pesquisa de filmes.

--- testesFrontend/cypress/support/steps/filme/pesquisaFilme.step.js
+++ testesFrontend/cypress/support/steps/filme/pesquisaFilme.step.js
@@ -1,7 +1,14 @@
 import { Given, When, Then, Before, After} from '@badeball/cypress-cucumber-preprocessor';
-
-//Importar aqui os Page Objects para desenvolver os testes
 import { faker } from '@faker-js/faker';
+
+import InicioPage from '../../pages/inicial.page'
+import DetalhesFilmePage from '../../pages/detalhesFilme.page';
+import LoginPage from'../../pages/login.page';
+
+
+const paginaInicial = new InicioPage();
+const paginaDetalhes = new DetalhesFilmePage();
+const paginaLogin = new LoginPage();
 
 Given ('', function () {
     
