b05996c96e0585db34b0a9a7e7c9fe3bd9eee987
Ivan-Coelho <ivancoelhobap@yahoo.com.br>
2024-06-14 08:17:27 -0300

    testes-avaliacao-filme

--- testesFrontend/cypress/support/steps/filme/avaliacaoFilme.step.js
+++ testesFrontend/cypress/support/steps/filme/avaliacaoFilme.step.js
@@ -41,6 +41,9 @@ Before({ tags: '@cadastroFilmeReview' }, function () {
     });
 });
 
+
+
+
 After({ tags: '@deletar' }, function () {
     cy.get('@userAdmin').then(function (userAdmin) {
         cy.get('@filme').then(function (response) {
@@ -52,37 +55,45 @@ After({ tags: '@deletar' }, function () {
 
 
 Given('que usuário logado acessa o site', function () {
-
+    cy.intercept('POST', 'https://raromdb-3c39614e42d4.herokuapp.com/api/auth/login').as('login')
     cy.criarUsuario().then(function (response) {
 
         let email = response.body.email
 
         cy.wrap(response.body).as('usuario');
-        cy.visit('/login')
-        paginaLogin.typeEmail(email)
-        paginaLogin.typeSenha('123456')
-        paginaLogin.clickButtonLogin()
+        cy.visit('/login');
+        paginaLogin.typeEmail(email);
+        paginaLogin.typeSenha('123456');
+        paginaLogin.clickButtonLogin();
+        cy.wait('@login');
     });
 });
 
 When('acessa a página de detalhes de um filme', function () {
     cy.get('@filme').then(function (filme) {
 
-        cy.intercept('GET', 'api/movies/search?*').as('pesquisa')
+        cy.intercept('GET', 'api/movies/search?*').as('pesquisa');
 
         paginaInicial.typeFilme(filme.title);
         paginaInicial.clickPesquisaFilme();
-        cy.wait('@pesquisa')
+        cy.wait('@pesquisa');
         paginaInicial.clickFilme();
-    })
+    });
 });
+
+// When('acessa a página de detalhes de um filme', function () {
+//     cy.get('@filme').then(function(response){
+//         idFilme = response.id
+//         cy.visit('/movies/' + idFilme)
+//     });
+// });
 
 When('informa uma nota', function () {
     paginaDetalhes.clickEstrela5();
 });
 
 When('informa um comentario para o filme', function () {
-    paginaDetalhes.typeReview('O filme é emocionante')
+    paginaDetalhes.typeReview('O filme é emocionante');
 });
 
 When('envia a avaliação do filme', function () {
@@ -90,25 +101,46 @@ When('envia a avaliação do filme', function () {
 
 });
 
+When('cria uma review válida para um filme que já tenha avaliado', function(){
+   cy.intercept('POST', 'https://raromdb-3c39614e42d4.herokuapp.com/api/users/review').as('espera')
+    paginaDetalhes.criarReview('da para assistir');
+    cy.wait('@espera')
+    cy.wait(100)
+    paginaDetalhes.typeReview('O filme é emocionante');
+    paginaDetalhes.clickEstrela5();
+    paginaDetalhes.clickButtonSalvar();
+    cy.wait('@espera')
+
+});
+
+When('informa um comentario grande para o filme', function () {
+    paginaDetalhes.typeReview('Star Wars é uma obra-prima cinematográfica que solidificou a saga como um fenômeno cultural. Lançado em 1980, o filme aprofunda a narrativa e os personagens, mostrando a luta desesperada da Aliança Rebelde contra o Império Galáctico. A revelação chocante de Darth Vader como pai de Luke Skywalker e o treinamento de Luke com Yoda são momentos icônicos. As cenas no planeta Hoth e na Cidade das Nuvens são visualmente deslumbrantes, e o desfecho sombrio deixa todos ansiosos pelo próximo capítulo. Bom');
+});
+
+When('informa um comentario gigante para o filme', function () {
+    paginaDetalhes.typeReview('Star Wars é uma obra-prima cinematográfica que solidificou a saga como um fenômeno cultural. Lançado em 1980, o filme aprofunda a narrativa e os personagens, mostrando a luta desesperada da Aliança Rebelde contra o Império Galáctico. A revelação chocante de Darth Vader como pai de Luke Skywalker e o treinamento de Luke com Yoda são momentos icônicos. As cenas no planeta Hoth e na Cidade das Nuvens são visualmente deslumbrantes, e o desfecho sombrio deixa todos ansiosos pelo próximo capítulo. BOOM');
+});
+
+
 Then('a avaliação do filme será criada com sucesso', function () {
 
     cy.get('@usuario').then(function (user) {
-        cy.log(user)
-        cy.contains(paginaDetalhes.nomeUsuario1, user.name)
-        cy.contains(paginaDetalhes.comentarioUsuario1, 'O filme é emocionante')
-        cy.get(paginaDetalhes.notausuario1).should('have.length', 5)
-        cy.get(paginaDetalhes.datausuario1).should('be.visible')
+        
+        cy.contains(paginaDetalhes.nomeUsuario1, user.name);
+        cy.contains(paginaDetalhes.comentarioUsuario1, 'O filme é emocionante');
+        cy.get(paginaDetalhes.notausuario1).should('have.length', 5);
+        cy.get(paginaDetalhes.datausuario1).should('be.visible');
     });
 });
 
 Then('a avaliação do filme será criada', function () {
 
     cy.get('@usuario').then(function (user) {
-        cy.log(user)
-        cy.contains(paginaDetalhes.nomeUsuario2, user.name)
-        cy.contains(paginaDetalhes.comentarioUsuario2, 'O filme é emocionante')
-        cy.get(paginaDetalhes.notausuario2).should('have.length', 5)
-        cy.get(paginaDetalhes.datausuario2).should('be.visible')
+       
+        cy.contains(paginaDetalhes.nomeUsuario2, user.name);
+        cy.contains(paginaDetalhes.comentarioUsuario2, 'O filme é emocionante');
+        cy.get(paginaDetalhes.notausuario2).should('have.length', 5);
+        cy.get(paginaDetalhes.datausuario2).should('be.visible');
     });
 });
 
@@ -126,3 +158,56 @@ Then('a nota de audiência será alterada para a média', function () {
         }
     });
 });
+
+Then('a avaliação do filme será criada com sucesso sem o comentários', function () {
+
+    cy.get('@usuario').then(function (user) {
+        
+        cy.contains(paginaDetalhes.nomeUsuario1, user.name);   
+        cy.get(paginaDetalhes.notausuario1).should('have.length', 5);
+        cy.get(paginaDetalhes.datausuario1).should('be.visible');
+    });
+});
+
+Then('o sistema deverá retornar uma mensagem de alerta informando que é necessário informar uma nota', function(){
+    cy.contains(paginaDetalhes.mensagemAlerta, 'Ocorreu um erro');
+    cy.contains(paginaDetalhes.textoMensagemAlerta,'Selecione uma estrela para avaliar o filme');
+});
+
+Then('a alteração do totalizador de nota deverá ser observada', function(){
+    // var data = new Date();    
+    // cy.contains( data.toLocaleDateString())
+    cy.get(paginaDetalhes.totalizadorAudiencia).should('have.length', 5)
+});
+
+Then('a review será atualizada', function(){
+    cy.get('@usuario').then(function (user) {
+       
+        cy.contains(paginaDetalhes.nomeUsuario1, user.name);
+        cy.contains(paginaDetalhes.comentarioUsuario1, 'O filme é emocionante');
+        cy.get(paginaDetalhes.notausuario1).should('have.length', 5);
+        cy.get(paginaDetalhes.datausuario1).should('be.visible');
+    });
+
+});
+
+Then('não será possível criar uma segunda review para o mesmo filme', function(){
+    cy.get(paginaDetalhes.totalReviewUsuarios).should('have.length', '1');
+});
+
+Then('a review do filme será criada com sucesso', function () {
+
+    cy.get('@usuario').then(function (user) {
+        
+        cy.contains(paginaDetalhes.nomeUsuario1, user.name);
+        cy.contains(paginaDetalhes.comentarioUsuario1, 'Star Wars é uma obra-prima cinematográfica que solidificou a saga como um fenômeno cultural. Lançado em 1980, o filme aprofunda a narrativa e os personagens, mostrando a luta desesperada da Aliança Rebelde contra o Império Galáctico. A revelação chocante de Darth Vader como pai de Luke Skywalker e o treinamento de Luke com Yoda são momentos icônicos. As cenas no planeta Hoth e na Cidade das Nuvens são visualmente deslumbrantes, e o desfecho sombrio deixa todos ansiosos pelo próximo capítulo. Bom');
+        cy.get(paginaDetalhes.notausuario1).should('have.length', 5);
+        cy.get(paginaDetalhes.datausuario1).should('be.visible');
+    });
+});
+
+Then('a review do filme não será criada com sucesso', function () {
+
+    cy.get(paginaDetalhes.reviewUsuarios).should('not.be.visible')
+});
+
