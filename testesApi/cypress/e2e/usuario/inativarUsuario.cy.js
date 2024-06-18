b2a43fb3ac13078f6e85ec3858ca58040052ded0
Ivan-Coelho <ivancoelhobap@yahoo.com.br>
2024-06-17 23:24:56 -0300

    update_final

--- testesApi/cypress/e2e/usuario/inativarUsuario.cy.js
+++ testesApi/cypress/e2e/usuario/inativarUsuario.cy.js
@@ -1,4 +1,4 @@
-describe ("Inativar Usuários", () => {
+describe("Inativar Usuários", () => {
     let usuario;
     let idFilme;
     let userAdmin;
@@ -8,16 +8,16 @@ describe ("Inativar Usuários", () => {
         cy.criarUsuarioAdmin().then(function (dadosAdmin) {
             userAdmin = dadosAdmin
             userAdmin.accessToken = dadosAdmin.tokenAdmin
-            cy.fixture('filmes/bodyReview.json').as('filme')
-            cy.cadastrarFilme(userAdmin.token).then(function (response) {
-                nameFilm = response.body.name
-                idFilme = response.body.id
-                this.filme.id = idFilme
-            })
+
         })
     });
 
     beforeEach(() => {
+        cy.cadastrarFilme(userAdmin.token).then(function (response) {
+            nameFilm = response.body.name
+            idFilme = response.body.id
+        });
+
         cy.criarUsuarioResponse().then((novo) => {
             usuario = novo
             cy.login(usuario).then((conteudo) => {
@@ -27,54 +27,35 @@ describe ("Inativar Usuários", () => {
     });
 
 
-    it ("Um usuário comum deve conseguir desativar sua conta", () => {
-            cy.inativarConta(usuario.accessToken).then((response) => {
-                expect(response.status).to.equal(204);
-                expect(response.body).to.equal("");
-        })
-    })
 
-    it ("Um administrador deve conseguir desativar uma conta", () => {
+
+    it("Um usuário comum deve conseguir desativar sua conta", () => {
         cy.inativarConta(usuario.accessToken).then((response) => {
             expect(response.status).to.equal(204);
             expect(response.body).to.equal("");
+        })
     })
+
+    it("Um administrador deve conseguir desativar uma conta", () => {
+        cy.inativarConta(usuario.accessToken).then((response) => {
+            expect(response.status).to.equal(204);
+            expect(response.body).to.equal("");
+        })
     })
 
 
-    it ("Um crítico deve conseguir desativar sua conta", () => {
+    it("Um crítico deve conseguir desativar sua conta", () => {
         cy.criarUsuarioCritico(usuario.accessToken).then(function () {
             cy.inativarConta(usuario.accessToken).then((response) => {
                 expect(response.status).to.equal(204);
                 expect(response.body).to.equal("");
-                })
-            });
+            })
         });
+    });
 
-    // it ("As informações de review de filme devem continuar mesmo com a inativação", () => {
-    //             cy.CadastrarFilme(userAdmin.accessToken)({
-    //                 title: " ",
-    //                 genre: " ",
-    //                 description: " ",
-    //                 durationInMinutes: 105,
-    //                 releaseYear: 2017
-    //             }, token).then((filme) => {
-    //                 filme = movie.body
-    //             }).then(function () {
-    //                 cy.criarReview(filme.id, 5, "Amei! Superou minhas expectativas", token).then(function () {
-    //                     cy.inativarConta(token);
+    
 
-    //                     cy.buscaFilmeId(filme.id).then((response) => {
-    //                         expect(response.status).to.equal(200);
-    //                         expect(response.body.reviews).to.be.an("array");
-    //                         expect(response.body.reviews[0].user.id).to.equal(usuario.id);
-    //                         expect(response.body.reviews[0].user.name).to.equal(usuario.name);
-    //                     })
-    //                 })
-    //             })
-    //         })
-
-    it ("Não deve ser possível inativar uma conta sem estar logado", () => {
+    it("Não deve ser possível inativar uma conta sem estar logado", () => {
         cy.request({
             method: 'PATCH',
             url: "/users/inactivate",
@@ -85,30 +66,89 @@ describe ("Inativar Usuários", () => {
             expect(response.body.error).to.be.eq('Unauthorized');
         });
     })
+
     
-    // it ("Deve ser possivel cadastrar usuario usando um e-mail de uma conta inativa", () => {
-        
-    //     let name = faker.person.fullName();
-    //     let password = "123456";
+});
 
-    //     cy.login(usuario).then((login) => {
-    //         token = login.body.accessToken
-    //     }).then(function () {
-    //         cy.inativarConta(token).then(() => {
-    //             cy.request({
-    //                 method: "POST",
-    //                 url: "/api/users/",
-    //                 body: {
-    //                     name: name,
-    //                     email: usuario.email,
-    //                     password: password,
-    //                 }
-    //             }).then((response) => {
-    //                 expect(response.status).to.equal(201);
-    //                 expect(response.body.email).to.deep.equal(usuarioCriado.email);
-    //             })
-    //         });
-    //     });
-    // })
 
-})
+describe('Cenário com Reviews', function () {
+    let userAdmin
+    let idFilme
+    before(function () {
+        cy.criarUsuarioAdmin().then(function (dadosAdmin) {
+            userAdmin = dadosAdmin
+            cy.cadastrarFilme(userAdmin.token).then(function (response) {
+                idFilme = response.body.id
+            });
+        });
+    });
+
+
+    after(function () {
+        cy.deletarFilme(idFilme, userAdmin.token);
+        cy.deletarUsuario(userAdmin.id, userAdmin.token);
+    });
+
+
+
+    it('Deve ser possível visualizar a review de um  usuário com conta inativada', function () {
+        let comentario
+        let nota
+        cy.usuarioLogado().then(function (dadosComum) {
+            let userComum = dadosComum
+            cy.criarReview(idFilme, userComum.token).then(function (response) {
+                comentario = response.comentario
+                nota = response.score
+
+                cy.request({
+                    method: "PATCH",
+                    url: "users/inactivate",
+                    headers: { Authorization: "Bearer " + userComum.token }
+                });
+
+                cy.buscaFilmeId(idFilme).then(function (response) {
+
+                    expect(response.id).to.equal(idFilme);
+                    expect(response.reviews[0].reviewText).to.equal(comentario);
+                    expect(response.reviews[0].reviewType).to.equal(0);
+                    expect(response.reviews[0].score).to.equal(nota);
+
+                    expect(response.reviews[0].user.id).to.equal(userComum.id);
+                    expect(response.reviews[0].user.name).to.equal(userComum.nome);
+                    expect(response.reviews[0].user.type).to.equal(0);
+                });
+            });
+        });
+    });
+
+    it('Uma vez inativado, o e-mail vinculado ao usuário se torna disponível', function () {
+        let userComum
+        cy.usuarioLogado().then(function (dadosComum) {
+            userComum = dadosComum
+            let email = dadosComum.email.toLowerCase()
+
+
+            cy.request({
+                method: "PATCH",
+                url: "users/inactivate",
+                headers: { Authorization: "Bearer " + userComum.token }
+            });
+
+            cy.request({
+                method: 'POST',
+                url: '/users',
+                body: {
+                    name: 'Fatima Leite',
+                    email: userComum.email,
+                    password: '123456'
+                },
+            }).then(function (response) {
+                expect(response.status).to.equal(201);
+                expect(response.body.email).to.equal(email)
+            });
+
+        });
+    });
+});
+
+
