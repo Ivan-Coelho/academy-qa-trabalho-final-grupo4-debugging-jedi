363ac98d9e4fc8f1097cc2389bd0338a193f99d7
Gabriel Vilela <116136811+GabrielVilela23@users.noreply.github.com>
2024-06-17 21:32:16 -0300

    Update inativarUsuario.cy.js

--- testesApi/cypress/e2e/usuario/inativarUsuario.cy.js
+++ testesApi/cypress/e2e/usuario/inativarUsuario.cy.js
@@ -49,29 +49,6 @@ describe("Inativar Usuários", () => {
     });
   });
 
-  // it ("As informações de review de filme devem continuar mesmo com a inativação", () => {
-  //             cy.CadastrarFilme(userAdmin.accessToken)({
-  //                 title: " ",
-  //                 genre: " ",
-  //                 description: " ",
-  //                 durationInMinutes: 105,
-  //                 releaseYear: 2017
-  //             }, token).then((filme) => {
-  //                 filme = movie.body
-  //             }).then(function () {
-  //                 cy.criarReview(filme.id, 5, "Amei! Superou minhas expectativas", token).then(function () {
-  //                     cy.inativarConta(token);
-
-  //                     cy.buscaFilmeId(filme.id).then((response) => {
-  //                         expect(response.status).to.equal(200);
-  //                         expect(response.body.reviews).to.be.an("array");
-  //                         expect(response.body.reviews[0].user.id).to.equal(usuario.id);
-  //                         expect(response.body.reviews[0].user.name).to.equal(usuario.name);
-  //                     })
-  //                 })
-  //             })
-  //         })
-
   it("Não deve ser possível inativar uma conta sem estar logado", () => {
     cy.request({
       method: "PATCH",
@@ -83,29 +60,4 @@ describe("Inativar Usuários", () => {
       expect(response.body.error).to.be.eq("Unauthorized");
     });
   });
-
-  // it ("Deve ser possivel cadastrar usuario usando um e-mail de uma conta inativa", () => {
-
-  //     let name = faker.person.fullName();
-  //     let password = "123456";
-
-  //     cy.login(usuario).then((login) => {
-  //         token = login.body.accessToken
-  //     }).then(function () {
-  //         cy.inativarConta(token).then(() => {
-  //             cy.request({
-  //                 method: "POST",
-  //                 url: "/api/users/",
-  //                 body: {
-  //                     name: name,
-  //                     email: usuario.email,
-  //                     password: password,
-  //                 }
-  //             }).then((response) => {
-  //                 expect(response.status).to.equal(201);
-  //                 expect(response.body.email).to.deep.equal(usuarioCriado.email);
-  //             })
-  //         });
-  //     });
-  // })
 });
